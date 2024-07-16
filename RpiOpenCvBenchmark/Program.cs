f68d2a66a376d9c363914dc7e3d8e979f386b3c2
Daniel Robaszkiewicz <daniel.robaszkiewicz@wp.pl>
2024-07-16 22:42:29 +0200

    test

--- RpiOpenCvBenchmark/Program.cs
+++ RpiOpenCvBenchmark/Program.cs
@@ -7,6 +7,8 @@ using Emgu.CV.Structure;
 using Microsoft.Diagnostics.Runtime.Utilities;
 using Emgu.CV.Util;
 using Microsoft.Diagnostics.Tracing.StackSources;
+using UMapx.Imaging;
+using Emgu.CV.Reg;
 
 namespace OpenCVBenchmarkRaspTest
 {
@@ -20,20 +22,25 @@ namespace OpenCVBenchmarkRaspTest
         private VectorOfMat _frames;
 
         private MergeMertens _merge_mertens;
-
+        private ExposureFusion _fusion;
+        private Bitmap[] _framesBitmap;
         [GlobalSetup]
         public void Setup()
         {
             _img = CvInvoke.Imread("lena.jpg");
             _faceDetector = new CascadeClassifier("haarcascade_frontalface_default.xml");
 
-            _firstFrame = CvInvoke.Imread("hdr1.png");
-            _secondFrame = CvInvoke.Imread("hdr2.png");
+            _firstFrame = CvInvoke.Imread("tig1.png");
+            _secondFrame = CvInvoke.Imread("tig2.png");
+         
             _frames = new VectorOfMat();
             _frames.Push(_firstFrame);
             _frames.Push(_secondFrame);
             _dstHdr = new Mat();
             _merge_mertens = new MergeMertens(1,0,0);
+            _fusion = new ExposureFusion(int.MaxValue);
+
+            _framesBitmap = new Bitmap[] { Emgu.CV.BitmapExtension.ToBitmap(_firstFrame), Emgu.CV.BitmapExtension.ToBitmap(_secondFrame) };
         }
 
      
@@ -49,13 +56,38 @@ namespace OpenCVBenchmarkRaspTest
                 CvInvoke.Rectangle(output, face, new MCvScalar(255, 255, 255));
             }
         }
+       
+        public void ExposureFusionTest()
+        {
+         
+            var result=_fusion.Apply(_framesBitmap);
+           
+        }
         [Benchmark]
+        public void SimpleHDR()
+        {
+            int width = _firstFrame.Width;
+            int height = _secondFrame.Height;
+
+            Image<Gray, Byte> firstImage = _firstFrame.ToImage<Gray, Byte>();
+            Image<Gray, Byte> secondImage = _secondFrame.ToImage<Gray, Byte>();
+            Image<Gray, Byte> dst = new Image<Gray, Byte>(width,height);
+            for (int col = 0; col < width; col++)
+            {
+                for (int row = 0; row < height; row++)
+                {
+                    var avg = (firstImage.Data[row, col, 0] + secondImage.Data[row, col, 0]) / 2.0;
+                    dst.Data[row, col, 0] = (byte)avg;
+                }
+            }
+           // dst.Save("simpleHDR.png");
+        }
         public void HdrTest()
         {
-          
+        
             _merge_mertens.Process(_frames, _dstHdr);
             _dstHdr = _dstHdr*255;
-
+            CvInvoke.Imwrite("OpenCVHdr.png", _dstHdr);
         }
     }
 
@@ -63,7 +95,12 @@ namespace OpenCVBenchmarkRaspTest
     {
         public static void Main(string[] args)
         {
-          
+            //var tmp = new OpenCVBenchmark();
+            //tmp.Setup();
+            //tmp.SimpleHDR();
+            //tmp.HdrTest();
+             // tmp.ExposureFusionTest();
+
              var summary = BenchmarkRunner.Run<OpenCVBenchmark>();
         }
     }
